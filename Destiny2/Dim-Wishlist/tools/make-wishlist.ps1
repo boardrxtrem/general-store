8946ade210188f9aff1b3e0fae3542a758a5df29
Boardrxtrem <boardrxtrem@live.com>
2024-07-07 18:51:28 -0700

    Add Heliocentric

--- Destiny2/Dim-Wishlist/tools/make-wishlist.ps1
+++ Destiny2/Dim-Wishlist/tools/make-wishlist.ps1
@@ -1,7 +1,6 @@
 $wishlist_name = "Boardrxtrem_Wishlist"
 $relative_wishlist_manager_output = ".\\littlelight\\dim\\$wishlist_name.txt"
 $relative_additional = ".\\additionals\\$wishlist_name.txt"
-
 $output = ".\$wishlist_name.txt"
 
 Copy-Item -Path $relative_wishlist_manager_output -Destination $output
